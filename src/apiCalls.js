525de70781bcbdebd5f809b5d0bf7b60f4da97b0
Adriane Sutherland <asutherland91@gmail.com>
2023-06-12 08:57:11 -0700

    Add post request

--- src/apiCalls.js
+++ src/apiCalls.js
@@ -2,3 +2,18 @@ export const getUrls = () => {
   return fetch('http://localhost:3001/api/v1/urls')
       .then(response => response.json())
 }
+
+export const postUrls = (url, title) => {
+  return fetch('http://localhost:3001/api/v1/urls', {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json"
+    },
+    body: {
+      long_url: url,
+      title: title
+    }
+  })
+  .then(response  => response.json());
+    
+}
