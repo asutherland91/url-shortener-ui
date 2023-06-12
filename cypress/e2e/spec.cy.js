bbc3167b900f4e672f3b129545366159e980a250
Adriane Sutherland <asutherland91@gmail.com>
2023-06-12 10:30:47 -0700

    Add tests

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -1,5 +1,18 @@
-describe('empty spec', () => {
-  it('passes', () => {
-    cy.visit('https://example.cypress.io')
-  })
-})
+describe("URL Shortener User Flows", () => {
+  context("when the API call is successful", () => {
+    beforeEach(() => {
+      cy.intercept("GET", "http://localhost:3001/api/v1/urls", {
+        statusCode: 200,
+        fixture: "urls.json",
+      }).as("urls");
+
+      cy.visit("localhost:3000");
+      cy.wait(["@urls"])
+    });
+
+    it("should display urls", () => {
+      cy.contains("h1", "URL Shortener")
+      cy.get(".url").should("have.length", 1);
+    });
+  });
+});
